6f69cd1ecac77b6e6fa09e71f6faa0f3aa5d22b1
00016334Frostyy <00016334wiut@gmail.com>
2024-12-05 10:19:17 +0500

    Added DAL

---
+++ WAD.BACKEND.16334/Controllers/QuestionsController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WAD.BACKEND._16334.Data;
+using WAD.BACKEND._16334.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WAD.BACKEND._16334.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly SurveyFormAppDbContext _context;
+
+        public QuestionsController(SurveyFormAppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Questions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        {
+            return await _context.Questions
+                                 .Include(q => q.Survey) 
+                                 .ToListAsync();
+        }
+
+        // GET: api/Questions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Question>> GetQuestion(int id)
+        {
+            var question = await _context.Questions
+                                          .Include(q => q.Survey)
+                                          .FirstOrDefaultAsync(q => q.id == id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return question;
+        }
+
+        // PUT: api/Questions/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuestion(int id, Question question)
+        {
+            if (id != question.id)
+            {
+                return BadRequest();
+            }
+
+            var survey = await _context.Surveys.FindAsync(question.SurveyId);
+            if (survey == null)
+            {
+                return NotFound(new { message = "Survey not found" });
+            }
+
+            question.Survey = survey; 
+
+            _context.Entry(question).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Questions
+        [HttpPost]
+        public async Task<ActionResult<Question>> PostQuestion(Question question)
+        {
+            var survey = await _context.Surveys.FindAsync(question.SurveyId);
+            if (survey == null)
+            {
+                return BadRequest(new { message = "Survey with the given ID does not exist." });
+            }
+
+            question.Survey = survey;
+            _context.Questions.Add(question);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuestion", new { id = question.id }, question);
+        }
+
+        // DELETE: api/Questions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuestion(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            _context.Questions.Remove(question);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool QuestionExists(int id)
+        {
+            return _context.Questions.Any(e => e.id == id);
+        }
+    }
+}
---
+++ WAD.BACKEND.16334/Controllers/SurveysController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WAD.BACKEND._16334.Data;
+using WAD.BACKEND._16334.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WAD.BACKEND._16334.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SurveysController : ControllerBase
+    {
+        private readonly SurveyFormAppDbContext _context;
+
+        public SurveysController(SurveyFormAppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Surveys
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Survey>>> GetSurveys()
+        {
+            return await _context.Surveys.ToListAsync();
+        }
+
+        // GET: api/Surveys/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Survey>> GetSurvey(int id)
+        {
+            var survey = await _context.Surveys.FindAsync(id);
+
+            if (survey == null)
+            {
+                return NotFound();
+            }
+
+            return survey;
+        }
+
+        // PUT: api/Surveys/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSurvey(int id, Survey survey)
+        {
+            if (id != survey.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(survey).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SurveyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Surveys
+        [HttpPost]
+        public async Task<ActionResult<Survey>> PostSurvey(Survey survey)
+        {
+            _context.Surveys.Add(survey);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSurvey", new { id = survey.id }, survey);
+        }
+
+        // DELETE: api/Surveys/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSurvey(int id)
+        {
+            var survey = await _context.Surveys.FindAsync(id);
+            if (survey == null)
+            {
+                return NotFound();
+            }
+
+            _context.Surveys.Remove(survey);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SurveyExists(int id)
+        {
+            return _context.Surveys.Any(e => e.id == id);
+        }
+    }
+}
